598793700da499ea04e88802706fad5538dbeb50
Julian Peffer <jepeffer@unomaha.edu>
2020-03-04 15:45:06 -0600

    Password and username

--- index.js
+++ index.js
@@ -30,15 +30,20 @@ app.get('/users', cors(), (req, res, next) => {
     con.query('SELECT * FROM GEE_DB.Users', function (error, results, fields) {
         if (error) throw error;
         console.log("This is req: " + req.query);
-        for (const key in req.query) {
-            console.log(key, req.query[key])
-          }
+   
+            pwd = req.query.password;
+            username = req.query.username;
+            console.log(pwd, username);
+          
         return res.send({ error: false, data: results, message: 'users list.' });
     });
 })
 
 
+verifyUser(username, password)
+{
 
+}
 
 
 con.connect(function(err) {
