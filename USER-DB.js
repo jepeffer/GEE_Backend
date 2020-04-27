abb88fad7b1e3ce827e356a14b3220279a80f190
Blaine Minden <bminden97@gmail.com>
2020-04-27 15:09:23 -0500

    Added keyword

--- USER-DB.js
+++ USER-DB.js
@@ -57,7 +57,7 @@ module.exports.getUsers = async (req) => {
   
 
   module.exports.search = async (req) => {
-    let query = "SELECT * FROM OER WHERE keyword LIKE %" + req + "%";
+    let query = "SELECT * FROM OER WHERE keyword LIKE %" + req.query.keywords + "%";
     let results = await pool.query(query);
     console.log("The results are " + results);
     return results;
