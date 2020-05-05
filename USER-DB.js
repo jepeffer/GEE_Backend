385439545a716679e1c895aa128e1a0e2a56e26a
Blaine Minden <bminden97@gmail.com>
2020-05-05 12:43:44 -0500

    Added Description

--- USER-DB.js
+++ USER-DB.js
@@ -89,9 +89,8 @@ module.exports.getUsers = async (req) => {
 
     console.log(CCCmatches);
     for(var x = 0; x < CCCmatches.length; x++){
-      if(CCCmatches[x] != undefined){
-        console.log(CCCmatches[x].description);
-      }
+      console.log(CCCmatches[x].description);
+      console.log(CCCmatches.length)
     }
 
     return results;
