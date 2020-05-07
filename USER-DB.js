0dbaa69af8abd4bed4146cccce9b8b5746e8f8b3
Julian Peffer <jepeffer@unomaha.edu>
2020-05-07 01:36:50 -0500

    Attempted fix

--- USER-DB.js
+++ USER-DB.js
@@ -146,7 +146,8 @@ module.exports.getUsers = async (req) => {
     var filelocation = "/root/Resources/" + fileTitle;
     let ziplocation = filelocation + ".zip";
     var dateobj = new Date();
-    var date_now = dateobj.toString();
+   // var date_now = dateobj.toString();
+    var date_now = "NOW";
     insert_statement = "INSERT INTO OER (userid, pdflocation, ziplocation, author, filelocation, description, name, subject, mediaformat, license, dateadded, grade, upvotes) VALUES" + 
     "(" + userid + ",\"" + pdflocation + "\"" + ",\"" + ziplocation + "\"" + ",\"" + username + "\"" +",\"" + filelocation + ",\"" + description + "\"" + ",\"" + fileTitle + "\"" + ",\"" + subject + "\"" + ",\"" + media_format + "\"" + ",\"" + license + "\"" + ",\"" + date_now + "\"" + ",\"" + grade + "\"," + 0 + ",\"" + tags + ",\"none\");"
     result = await pool.query(insert_statement);
