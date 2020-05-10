da73807be17fd9378d066fb96980aa5405f46e62
Julian Peffer <jepeffer@unomaha.edu>
2020-05-09 23:16:16 -0500

    New functions

--- index.js
+++ index.js
@@ -212,9 +212,9 @@ app.get('/download',cors(), function(req, res){
   console.log("Downloading called");
   filelocation = req.query.filelocation.toString();
   console.log("Now downloading: " + filelocation);
-  filenamelist = filelocation.split("/");
+  filenamelist = filelocation.split('/');
   filename = filelocation[filelocation.length - 1];
-    res.download(filename + ".zip", filelocation); // Set disposition and send it.
+    res.download(filelocation, filename + ".zip"); // Set disposition and send it.
   });
 
 /*app.get('/users', cors(), (req, res, next) => {
