7a8da1c3f2d57baa27097432cfb74b0ebce5d1b6
Julian Peffer <jepeffer@unomaha.edu>
2020-05-09 23:37:46 -0500

    New functions

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ app.get('/api', function (req, res) {
 app.post('/api/upload', cors(), upload.single('file'), function (req, res) {
   console.log("File recieved " + req.file.originalname);
   db.uploadFile(req, res).then(result => {
+    console.log("This was the result: " + res.toString());
     if (res.toString() == "Bad")
     {
       return res.send({
